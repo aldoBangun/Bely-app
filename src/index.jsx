4245e08a6ad07fbdfe89cdf8166b4024827dda4a
aldoBangun <punk.aldo68@gmail.com>
2022-09-17 15:44:06 +0800

    Add theme provider and css baseline

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,12 @@
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import theme from './theme'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>
+)
