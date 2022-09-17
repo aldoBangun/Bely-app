bfe7503f26f977d03f4e34550fc8faf72340d7aa
aldoBangun <punk.aldo68@gmail.com>
2022-09-17 15:42:52 +0800

    add custom theme

---
+++ src/theme.js
@@ -0,0 +1,22 @@
+import { createTheme } from '@mui/material'
+
+export default createTheme({
+  spacing: 8,
+  palette: {
+    primary: {
+      main: '#DB3022'
+    },
+    error: {
+      main: '#D84242'
+    },
+    success: {
+      main: '#2AA952'
+    },
+    info: {
+      main: '#4290D8'
+    },
+    warning: {
+      main: '#FFBA49'
+    }
+  }
+})
